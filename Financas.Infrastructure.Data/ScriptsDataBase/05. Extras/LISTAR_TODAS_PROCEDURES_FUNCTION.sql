7c6395cf509a4cdbe0e5eee113108ccd80451ca4
João Fama Guimarães Junior <joaofama@gmail.com>
2018-02-26 23:53:50 -0300

    Script Database

--- Financas.Infrastructure.Data/ScriptsDataBase/05. Extras/LISTAR_TODAS_PROCEDURES_FUNCTION.sql
+++
@@ -1,8 +0,0 @@
-select * 
-  from Financas.information_schema.routines 
- where routine_type = 'PROCEDURE'
- 
- 
- select * 
-  from Financas.information_schema.routines 
- where routine_type = 'FUNCTION'
